c4af669c47e047ff6aa7e3b43a982ec1dd96404c
Mahmoud Khashaba <54bao.o@gmail.com>
2024-08-30 20:36:19 +0000

    updated get common

--- app/views/get_common.py
+++ app/views/get_common.py
@@ -2,53 +2,74 @@ from django.http import JsonResponse
 from adrf.views import APIView
 from app.pagination import StandardResultsSetPagination
 from app.db_models.parent_user import ParentUser
+from neomodel import db
 import logging
+from asgiref.sync import sync_to_async
+from ..middlewares.custom_token_auth import CustomTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 logger = logging.getLogger('app')
 
-async def get_common_items(user, bud_node, item_type):
-    user_likes = {}
-    bud_likes = {}
+class CommonItemsMixin:
+    authentication_classes = [CustomTokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-    for account in user.associated_accounts.values():
-        if account is not None and hasattr(account, 'get_likes'):
-            account_likes = await account.get_likes() or {}
-            for key, value in account_likes.items():
-                if key not in user_likes:
-                    user_likes[key] = []
-                user_likes[key].extend(value)
+    @method_decorator(csrf_exempt)
+    async def dispatch(self, *args, **kwargs):
+        return await super().dispatch(*args, **kwargs)
 
-    accounts = await bud_node.associated_accounts()
-    for account in accounts.values():
-        if account is not None and hasattr(account, 'get_likes'):
-            account_likes = await account.get_likes() or {}
-            for key, value in account_likes.items():
-                if key not in bud_likes:
-                    bud_likes[key] = []
-                bud_likes[key].extend(value)
+    async def get_common_items(self, user, bud_node, item_type):
+        query = """
+        MATCH (user:ParentUser {uid: $user_uid})-[:CONNECTED_TO_SPOTIFY|CONNECTED_TO_YTMUSIC|CONNECTED_TO_LASTFM|CONNECTED_TO_MAL]->()-[r1]->(item)
+        MATCH (bud:ParentUser {uid: $bud_uid})-[:CONNECTED_TO_SPOTIFY|CONNECTED_TO_YTMUSIC|CONNECTED_TO_LASTFM|CONNECTED_TO_MAL]->()-[r2]->(item)
+        WHERE (
+            CASE $item_type
+                WHEN 'Artist' THEN item:Artist AND type(r1) IN ['LIKES_ARTIST', 'TOP_ARTIST'] AND type(r2) IN ['LIKES_ARTIST', 'TOP_ARTIST']
+                WHEN 'Track' THEN item:Track AND type(r1) IN ['LIKES_TRACK', 'TOP_TRACK', 'PLAYED_TRACK'] AND type(r2) IN ['LIKES_TRACK', 'TOP_TRACK', 'PLAYED_TRACK']
+                WHEN 'Genre' THEN item:Genre AND type(r1) = 'LIKES_GENRE' AND type(r2) = 'LIKES_GENRE'
+                WHEN 'Album' THEN item:Album AND type(r1) = 'LIKES_ALBUM' AND type(r2) = 'LIKES_ALBUM'
+                WHEN 'Anime' THEN item:Anime AND type(r1) = 'LIKES_ANIME' AND type(r2) = 'LIKES_ANIME'
+                WHEN 'Manga' THEN item:Manga AND type(r1) = 'LIKES_MANGA' AND type(r2) = 'LIKES_MANGA'
+                ELSE false
+            END
+        )
+        RETURN DISTINCT item
+        """
+        params = {
+            'user_uid': user.uid,
+            'bud_uid': bud_node.uid,
+            'item_type': item_type
+        }
+        
+        results, _ = await sync_to_async(db.cypher_query)(query, params)
+        
+        if not results:
+            return None
+        
+        common_items = [item[0] for item in results]
+        
+        serialized_items = []
+        for item in common_items:
+            if hasattr(item, 'serialize'):
+                serialized_item = await sync_to_async(item.serialize)()
+            else:
+                serialized_item = dict(item)
+            serialized_items.append(serialized_item)
 
-    if not user_likes or not bud_likes:
-        return None
+        return serialized_items
 
-    common_items = user.get_common_items(
-        bud_likes.get(item_type, []),
-        user_likes.get(item_type, [])
-    )
+    async def paginate_response(self, request, items):
+        paginator = StandardResultsSetPagination()
+        paginated_items = paginator.paginate_queryset(items, request)
+        response_data = paginator.get_paginated_response(paginated_items)
+        return response_data
 
-    serialized_items = [
-        await item.serialize() if hasattr(item, 'serialize') else item
-        for item in common_items
-    ]
-
-    return serialized_items
-
-class CommonItemsView(APIView):
+class CommonItemsView(CommonItemsMixin, APIView):
     item_type = None
 
     async def post(self, request):
-        return await self.get_common_items(request)
-
-    async def get_common_items(self, request):
         try:
             user = request.user
             bud_id = request.data.get('bud_id')
@@ -62,17 +83,15 @@ class CommonItemsView(APIView):
                 logger.warning(f'User or bud not found: user={user}, bud_id={bud_id}')
                 return JsonResponse({'error': 'User or bud not found'}, status=404)
 
-            common_items = await get_common_items(user, bud_node, self.item_type)
+            common_items = await self.get_common_items(user, bud_node, self.item_type)
 
             if common_items is None:
-                logger.warning('User likes or bud likes returned empty.')
-                return JsonResponse({'error': 'No likes found for user or bud.'}, status=404)
+                logger.warning('No common items found.')
+                return JsonResponse({'error': 'No common items found.'}, status=404)
 
-            paginator = StandardResultsSetPagination()
-            paginated_items = paginator.paginate_queryset(common_items, request)
-            response_data = paginator.get_paginated_response(paginated_items)
+            response_data = await self.paginate_response(request, common_items)
 
-            logger.info('Successfully fetched common %s data for user=%s, bud_id=%s', self.item_type, user.uid, bud_id)
+            logger.info(f'Successfully fetched common {self.item_type} data for user={user.uid}, bud_id={bud_id}')
             return JsonResponse(response_data, safe=False)
 
         except Exception as e:
@@ -81,59 +100,32 @@ class CommonItemsView(APIView):
             return JsonResponse({'error': 'Internal Server Error', 'type': error_type}, status=500)
 
 class GetCommonLikedArtists(CommonItemsView):
-    item_type = 'likes_artists'
-
-    async def post(self, request):
-        return await super().get_common_items(request)
+    item_type = 'Artist'
 
 class GetCommonLikedTracks(CommonItemsView):
-    item_type = 'likes_tracks'
-
-    async def post(self, request):
-        return await super().get_common_items(request)
+    item_type = 'Track'
     
 class GetCommonLikedGenres(CommonItemsView):
-    item_type = 'likes_genres'
-
-    async def post(self, request):
-        return await super().get_common_items(request)
-
+    item_type = 'Genre'
 
 class GetCommonLikedAlbums(CommonItemsView):
-    item_type = 'likes_albums'
-
-    async def post(self, request):
-        return await super().get_common_items(request)
+    item_type = 'Album'
 
 class GetCommonPlayedTracks(CommonItemsView):
-    item_type = 'played_tracks'
+    item_type = 'Track'
 
-    async def post(self, request):
-        return await super().get_common_items(request)
-    
 class GetCommonTopArtists(CommonItemsView):
-    item_type = 'top_artists'
-
-    async def post(self, request):
-        return await super().get_common_items(request)
-
+    item_type = 'Artist'
 
 class GetCommonTopTracks(CommonItemsView):
-    item_type = 'top_tracks'
-    async def post(self, request):
-        return await super().get_common_items(request)
+    item_type = 'Track'
 
 class GetCommonTopGenres(CommonItemsView):
-    item_type = 'top_genres'
-    async def post(self, request):
-        return await super().get_common_items(request)
-    
+    item_type = 'Genre'
+
 class GetCommonTopAnime(CommonItemsView):
-    item_type = 'top_anime'
-    async def post(self, request):
-        return await super().get_common_items(request)
+    item_type = 'Anime'
+
 class GetCommonTopManga(CommonItemsView):
-    item_type = 'top_manga'
-    async def post(self, request):
-        return await super().get_common_items(request)
+    item_type = 'Manga'
 
